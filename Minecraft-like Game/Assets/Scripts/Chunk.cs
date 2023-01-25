980433fd4007bf71104ef3a0ebe4ef89f0ca8426
Andrzej Kebab <grzesiugora123@wp.pl>
2023-01-25 02:24:22 +0100

    Revert "Redo, Jobs and Burst"
    This reverts commit d64d40d69d6e0207c8d1044c644750ead8c1114d.

--- Minecraft-like Game/Assets/Scripts/Chunk.cs
+++ Minecraft-like Game/Assets/Scripts/Chunk.cs
@@ -1,78 +1,300 @@
-using System.Linq;
-using Unity.Collections;
-using Unity.Jobs;
-using Unity.Mathematics;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class Chunk : MonoBehaviour
+public class Chunk 
 {
-	private  MeshFilter meshFilter;
+	[SerializeField] private ChunkCoord coord;
 
-	private void Awake()
+	private GameObject chunkObject;
+	private MeshRenderer meshRenderer;
+	private MeshFilter meshFilter;
+
+	private int vertexIndex = 0;
+	private List<Vector3> vertices = new List<Vector3> ();
+	private List<int> triangles = new List<int> ();
+	private List<int> transparentTriangles = new List<int>();
+	private Material[] materials = new Material[2];
+	private List<Vector2> uvs = new List<Vector2> ();
+
+	public byte[,,] VoxelMap = new byte[VoxelData.ChunkWidth, VoxelData.ChunkHeight, VoxelData.ChunkWidth];
+
+	public Queue<VoxelMod> modifications = new Queue<VoxelMod>();
+
+	private World world;
+
+	private bool _isActive;
+	public bool IsVoxelMapPopulated = false;
+
+	public Chunk (ChunkCoord _coord, World _world, bool generateOnLoad) 
 	{
-		meshFilter = GetComponent<MeshFilter>();
+		coord = _coord;
+		world = _world;
+		isActive = true;
+
+		if (generateOnLoad)
+			Init();
 	}
 
-	private void Start()
+	public void Init () 
 	{
-		var _position = transform.position;
+		chunkObject = new GameObject();
+		meshFilter = chunkObject.AddComponent<MeshFilter>();
+		meshRenderer = chunkObject.AddComponent<MeshRenderer>();
 
-		var blocks = new NativeArray<Block>(4096, Allocator.TempJob);
+		materials[0] = world.Material;
+		materials[1] = world.TransparentMaterial;
+		meshRenderer.materials = materials;
 
-		for(int x = 0; x < 16; x++)
+		chunkObject.transform.SetParent(world.transform);
+		chunkObject.transform.position = new Vector3(coord.x * VoxelData.ChunkWidth, 0f, coord.z * VoxelData.ChunkWidth);
+		chunkObject.name = "Chunk " + coord.x + ", " + coord.z;
+
+		PopulateVoxelMap();
+		UpdateChunk();
+	}
+
+	private void PopulateVoxelMap () 
+	{
+		for (int y = 0; y < VoxelData.ChunkHeight; y++) 
 		{
-			for (int z = 0; z < 16; z++)
+			for (int x = 0; x < VoxelData.ChunkWidth; x++) 
 			{
-				var y = Mathf.FloorToInt(Mathf.PerlinNoise((_position.x + x) * 0.15f, (_position.z + z) * 0.15f) * 16);
-
-				for (int i = 0; i < y; i++)
+				for (int z = 0; z < VoxelData.ChunkWidth; z++) 
 				{
-					blocks[BlockExtensions.GetBlockIndex(new int3(x, i, z))] = Block.Stone;
-				}
-
-				for (int i = y; i < 16; i++)
-				{
-					blocks[BlockExtensions.GetBlockIndex(new int3(x, i, z))] = Block.Air;
+					VoxelMap[x, y, z] = world.GetVoxel(new Vector3(x, y, z) + position);
 				}
 			}
 		}
 
-		var meshData = new ChunkJob.MeshData()
-		{
-			Vertices = new NativeList<int3>(Allocator.TempJob),
-			Triangles = new NativeList<int>(Allocator.TempJob)
-		};
+		IsVoxelMapPopulated = true;
 
-		var jobHandle = new ChunkJob
+	}
+
+	public void UpdateChunk () 
+	{
+		while(modifications.Count > 0)
 		{
-			meshData = meshData,
-			chunkData = new ChunkJob.ChunkData
+			VoxelMod v = modifications.Dequeue();
+			Vector3 pos = v.Position -= position;
+			VoxelMap[(int)pos.x, (int)pos.y, (int)pos.z] = v.ID;
+		}
+
+		ClearMeshData();
+
+		for (int y = 0; y < VoxelData.ChunkHeight; y++) 
+		{
+			for (int x = 0; x < VoxelData.ChunkWidth; x++) 
 			{
-				Blocks = blocks
-			},
-			blockData = new ChunkJob.BlockData
-			{
-				Vertices = VoxelData.Vertices,
-				Triangles = VoxelData.Triangles
+				for (int z = 0; z < VoxelData.ChunkWidth; z++)
+				{
+					if (world.BlockTypes[VoxelMap[x,y,z]].IsSolid)
+						UpdateMeshData (new Vector3(x, y, z));
+				}
 			}
-		}.Schedule();
+		}
+		CreateMesh();
+	}
 
-		jobHandle.Complete();
+	private void ClearMeshData()
+	{
+		vertexIndex = 0;
+		vertices.Clear();
+		triangles.Clear();
+		transparentTriangles.Clear();
+		uvs.Clear();
+	}
 
-		Mesh mesh = new Mesh();
-		mesh.MarkDynamic();
-		mesh.vertices = meshData.Vertices.ToArray().Select(vertex => new Vector3(vertex.x, vertex.y, vertex.z)).ToArray();
-		mesh.triangles = meshData.Triangles.ToArray();
-		mesh.RecalculateNormals();
-		mesh.RecalculateBounds();
-		mesh.RecalculateTangents();
-		mesh.RecalculateUVDistributionMetrics();
-		mesh.Optimize();
+	public bool isActive 
+	{
+		get { return _isActive; }
+		set {
+			_isActive = value;
+			if (chunkObject != null)
+				chunkObject.SetActive(value);
+		}
+	}
+
+	public Vector3 position
+	{
+		get { return chunkObject.transform.position; }
+	}
+
+	private bool IsVoxelInChunk (int x, int y, int z) 
+	{
+		if (x < 0 || x > VoxelData.ChunkWidth - 1 || y < 0 || y > VoxelData.ChunkHeight - 1 || z < 0 || z > VoxelData.ChunkWidth - 1)
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+	}
+
+	public void EditVoxel(Vector3 pos, byte newID)
+	{
+		int xCheck = Mathf.FloorToInt(pos.x);
+		int yCheck = Mathf.FloorToInt(pos.y);
+		int zCheck = Mathf.FloorToInt(pos.z);
+
+		xCheck -= Mathf.FloorToInt(chunkObject.transform.position.x);
+		zCheck -= Mathf.FloorToInt(chunkObject.transform.position.z);
+
+		VoxelMap[xCheck, yCheck, zCheck] = newID;
+
+		UpdateSurroundingVoxel(xCheck, yCheck, zCheck);
+		UpdateChunk();
+	}
+
+	private void UpdateSurroundingVoxel(int x, int y, int z)
+	{
+		Vector3 thisVoxel = new Vector3(x, y, z);
+
+		for(int p = 0; p < 6; p++)
+		{
+			Vector3 currentVoxel = thisVoxel + VoxelData.FaceChecks[p];
+
+			if (!IsVoxelInChunk((int)currentVoxel.x, (int)currentVoxel.y, (int)currentVoxel.z))
+			{
+				world.GetChunkFromVector3(currentVoxel + position).UpdateChunk();
+			}
+		}
+	}
+
+	private bool CheckVoxel (Vector3 pos) 
+	{
+		int x = Mathf.FloorToInt (pos.x);
+		int y = Mathf.FloorToInt (pos.y);
+		int z = Mathf.FloorToInt (pos.z);
+
+		if (!IsVoxelInChunk(x, y, z)) return world.CheckIfVoxelTransparent(pos + position);
+
+		return world.BlockTypes[VoxelMap [x, y, z]].IsTransparent;
+	}
+
+	public byte GetVoxelFromGlobalVector3 (Vector3 pos) 
+	{
+		int xCheck = Mathf.FloorToInt(pos.x);
+		int yCheck = Mathf.FloorToInt(pos.y);
+		int zCheck = Mathf.FloorToInt(pos.z);
+
+		xCheck -= Mathf.FloorToInt(chunkObject.transform.position.x);
+		zCheck -= Mathf.FloorToInt(chunkObject.transform.position.z);
+
+		return VoxelMap[xCheck, yCheck, zCheck];
+	}
+
+	private void UpdateMeshData (Vector3 pos) 
+	{
+		byte BlockID = VoxelMap[(int)pos.x, (int)pos.y, (int)pos.z];
+		bool _isTransparent = world.BlockTypes[BlockID].IsTransparent;
+
+		for (int p = 0; p < 6; p++) 
+		{ 
+			if (CheckVoxel(pos + VoxelData.FaceChecks[p])) 
+			{
+				vertices.Add (pos + VoxelData.VoxelVerticles [VoxelData.VoxelTriangles [p, 0]]);
+				vertices.Add (pos + VoxelData.VoxelVerticles [VoxelData.VoxelTriangles [p, 1]]);
+				vertices.Add (pos + VoxelData.VoxelVerticles [VoxelData.VoxelTriangles [p, 2]]);
+				vertices.Add (pos + VoxelData.VoxelVerticles [VoxelData.VoxelTriangles [p, 3]]);
+
+				AddTexture(world.BlockTypes[BlockID].GetTextureID(p));
+
+				if(!_isTransparent)
+				{
+					triangles.Add(vertexIndex);
+					triangles.Add(vertexIndex + 1);
+					triangles.Add(vertexIndex + 2);
+					triangles.Add(vertexIndex + 2);
+					triangles.Add(vertexIndex + 1);
+					triangles.Add(vertexIndex + 3);
+				}
+				else
+				{
+					transparentTriangles.Add(vertexIndex);
+					transparentTriangles.Add(vertexIndex + 1);
+					transparentTriangles.Add(vertexIndex + 2);
+					transparentTriangles.Add(vertexIndex + 2);
+					transparentTriangles.Add(vertexIndex + 1);
+					transparentTriangles.Add(vertexIndex + 3);
+				}
+
+				vertexIndex += 4;
+			}
+		}
+	}
+
+	private void CreateMesh () 
+	{
+		Mesh mesh = new Mesh ();
+		mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
+		mesh.vertices = vertices.ToArray ();
+		mesh.subMeshCount = 2;
+		mesh.SetTriangles(triangles.ToArray(), 0);
+		mesh.SetTriangles(transparentTriangles.ToArray(), 1);
+		mesh.uv = uvs.ToArray ();
+
+		mesh.RecalculateNormals ();
 
 		meshFilter.mesh = mesh;
+	}
 
-		meshData.Triangles.Dispose();
-		meshData.Vertices.Dispose();
-		blocks.Dispose();
+	private void AddTexture (int textureID)
+	{
+		float y = textureID / VoxelData.TextureAtlasSizeInBlocks;
+		float x = textureID - (y * VoxelData.TextureAtlasSizeInBlocks);
+
+		x *= VoxelData.NormalizedBlockTextureSize;
+		y *= VoxelData.NormalizedBlockTextureSize;
+
+		y = 1f - y - VoxelData.NormalizedBlockTextureSize;
+
+		uvs.Add(new Vector2(x, y));
+		uvs.Add(new Vector2(x, y + VoxelData.NormalizedBlockTextureSize));
+		uvs.Add(new Vector2(x + VoxelData.NormalizedBlockTextureSize, y));
+		uvs.Add(new Vector2(x + VoxelData.NormalizedBlockTextureSize, y + VoxelData.NormalizedBlockTextureSize));
+	}
+}
+
+public class ChunkCoord {
+
+	public int x;
+	public int z;
+
+	public ChunkCoord () 
+	{
+		x = 0;
+		z = 0;
+	}
+
+	public ChunkCoord (int _x, int _z) 
+	{
+		x = _x;
+		z = _z;
+	}
+
+	public ChunkCoord (Vector3 pos) 
+	{
+		int xCheck = Mathf.FloorToInt(pos.x);
+		int zCheck = Mathf.FloorToInt(pos.z);
+
+		x = xCheck / VoxelData.ChunkWidth;
+		z = zCheck / VoxelData.ChunkWidth;
+	}
+
+	public bool Equals (ChunkCoord other)
+	{
+		if (other == null)
+		{
+			return false;
+		}
+		else if (other.x == x && other.z == z)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
 	}
 }
---
+++ Minecraft-like Game/Assets/Scripts/Player.cs
@@ -0,0 +1,244 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	private bool IsGrounded;
+	private bool IsSprinting;
+
+	private Transform cam;
+	private World world;
+
+	[Header("Player Data")]
+	[SerializeField] private float walkSpeed = 3f;
+	[SerializeField] private float sprintSpeed = 6f;
+	[SerializeField] private float jumpForce = 5f;
+	[SerializeField] private float gravity = -9.8f;
+	[SerializeField] private float playerWidth = 0.15f;
+
+	private float horizontal;
+	private float vertical;
+	private float mouseHorizontal;
+	private float mouseVertical;
+	private Vector3 velocity;
+	private float verticalMomentum = 0;
+	private bool jumpRequest;
+
+	[SerializeField] private Transform highlightBlock;
+	[SerializeField] private Transform placeBlock;
+	[SerializeField] private float checkIncrement = 0.01f;
+	[SerializeField] private float reach = 8f;
+
+	public byte SelectedBlockIndex = 1;
+
+	private void Start() 
+	{
+		cam = GameObject.Find("Main Camera").transform;
+		world = GameObject.Find("World").GetComponent<World>();
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
+	private void FixedUpdate()
+	{
+		CalculateVelocity();
+
+		if (jumpRequest) Jump();
+
+		transform.Translate(velocity, Space.World);
+	}
+
+	private void Update()
+	{
+		GetPlayerInputs();
+		PlaceCursorBlocks();
+
+		transform.Rotate(Vector3.up * mouseHorizontal * 2.5f);
+		cam.Rotate(Vector3.right * -mouseVertical * 2.5f);
+	}
+
+	private void Jump() 
+	{
+		verticalMomentum = jumpForce;
+		IsGrounded = false;
+		jumpRequest = false;
+	}
+
+	private void CalculateVelocity()
+	{
+		if (verticalMomentum > gravity)
+			verticalMomentum += Time.fixedDeltaTime * gravity;
+
+		if (IsSprinting)
+			velocity = ((transform.forward * vertical) + (transform.right * horizontal)) * Time.fixedDeltaTime * sprintSpeed;
+		else
+			velocity = ((transform.forward * vertical) + (transform.right * horizontal)) * Time.fixedDeltaTime * walkSpeed;
+
+		velocity += Vector3.up * verticalMomentum * Time.fixedDeltaTime;
+
+		if ((velocity.z > 0 && Front) || (velocity.z < 0 && Back))
+			velocity.z = 0;
+		if ((velocity.x > 0 && Right) || (velocity.x < 0 && Left))
+			velocity.x = 0;
+
+		if (velocity.y < 0)
+			velocity.y = CheckDownSpeed(velocity.y);
+		else if (velocity.y > 0)
+			velocity.y = CheckUpSpeed(velocity.y);
+	}
+
+	private void GetPlayerInputs()
+	{
+		horizontal = Input.GetAxis("Horizontal");
+		vertical = Input.GetAxis("Vertical");
+		mouseHorizontal = Input.GetAxis("Mouse X");
+		mouseVertical = Input.GetAxis("Mouse Y");
+
+		if (Input.GetButtonDown("Sprint")) IsSprinting = true;
+
+		if (Input.GetButtonUp("Sprint")) IsSprinting = false;
+
+		if (IsGrounded && Input.GetButtonDown("Jump")) jumpRequest = true;
+
+		if(highlightBlock.gameObject.activeSelf)
+		{
+			if(Input.GetMouseButtonDown(0))
+			{
+				world.GetChunkFromVector3(highlightBlock.position).EditVoxel(highlightBlock.position, 0);
+			}
+
+			if (Input.GetMouseButtonDown(1))
+			{
+				world.GetChunkFromVector3(placeBlock.position).EditVoxel(placeBlock.position, SelectedBlockIndex);
+			}
+		}
+	}
+
+	private void PlaceCursorBlocks()
+	{
+		float _step = checkIncrement;
+		Vector3 _lastPos = new Vector3();
+
+		while(_step < reach)
+		{
+			Vector3 pos = cam.position + (cam.forward * _step);
+
+			if(world.CheckForVoxel(pos))
+			{
+				highlightBlock.position = new Vector3(Mathf.FloorToInt(pos.x), Mathf.FloorToInt(pos.y), Mathf.FloorToInt(pos.z));
+				placeBlock.position = _lastPos;
+
+				highlightBlock.gameObject.SetActive(true);
+				placeBlock.gameObject.SetActive(true);
+
+				return;
+			}
+
+			_lastPos = new Vector3(Mathf.FloorToInt(pos.x), Mathf.FloorToInt(pos.y), Mathf.FloorToInt(pos.z));
+
+			_step += checkIncrement;
+		}
+
+		highlightBlock.gameObject.SetActive(false);
+		placeBlock.gameObject.SetActive(false);
+	}
+
+	private float CheckDownSpeed(float downSpeed) 
+	{
+		if (
+			world.CheckForVoxel(new Vector3(transform.position.x - playerWidth, transform.position.y + downSpeed, transform.position.z - playerWidth)) ||
+			world.CheckForVoxel(new Vector3(transform.position.x + playerWidth, transform.position.y + downSpeed, transform.position.z - playerWidth)) ||
+			world.CheckForVoxel(new Vector3(transform.position.x + playerWidth, transform.position.y + downSpeed, transform.position.z + playerWidth)) ||
+			world.CheckForVoxel(new Vector3(transform.position.x - playerWidth, transform.position.y + downSpeed, transform.position.z + playerWidth))
+		   ) 
+		{
+			IsGrounded = true;
+			return 0;
+		} 
+		else 
+		{
+			IsGrounded = false;
+			return downSpeed;
+		}
+	}
+
+	private float CheckUpSpeed(float upSpeed) 
+	{
+		if (
+			world.CheckForVoxel(new Vector3(transform.position.x - playerWidth, transform.position.y + 2f + upSpeed, transform.position.z - playerWidth)) ||
+			world.CheckForVoxel(new Vector3(transform.position.x + playerWidth, transform.position.y + 2f + upSpeed, transform.position.z - playerWidth)) ||
+			world.CheckForVoxel(new Vector3(transform.position.x + playerWidth, transform.position.y + 2f + upSpeed, transform.position.z + playerWidth)) ||
+			world.CheckForVoxel(new Vector3(transform.position.x - playerWidth, transform.position.y + 2f + upSpeed, transform.position.z + playerWidth))
+		   ) 
+		{
+			return 0;
+		} 
+		else 
+		{
+			return upSpeed;
+		}
+	}
+
+	public bool Front 
+	{
+		get {
+			if (
+				world.CheckForVoxel(new Vector3(transform.position.x, transform.position.y, transform.position.z + playerWidth)) ||
+				world.CheckForVoxel(new Vector3(transform.position.x, transform.position.y + 1f, transform.position.z + playerWidth))
+				)
+				return true;
+			else
+				return false;
+		}
+	}
+
+	public bool Back 
+	{
+		get {
+			if (
+				world.CheckForVoxel(new Vector3(transform.position.x, transform.position.y, transform.position.z - playerWidth)) ||
+				world.CheckForVoxel(new Vector3(transform.position.x, transform.position.y + 1f, transform.position.z - playerWidth))
+				)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+
+	public bool Left 
+	{
+		get {
+			if (
+				world.CheckForVoxel(new Vector3(transform.position.x - playerWidth, transform.position.y, transform.position.z)) ||
+				world.CheckForVoxel(new Vector3(transform.position.x - playerWidth, transform.position.y + 1f, transform.position.z))
+				)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+
+	public bool Right 
+	{
+		get {
+			if (
+				world.CheckForVoxel(new Vector3(transform.position.x + playerWidth, transform.position.y, transform.position.z)) ||
+				world.CheckForVoxel(new Vector3(transform.position.x + playerWidth, transform.position.y + 1f, transform.position.z))
+				)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+}
